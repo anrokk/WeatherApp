e24a4b56f0201b9c8de3e8a10c118df60583361e
anrokk <anrokk@taltech.ee>
2024-06-03 14:25:28 +0300

    API fixes

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -21,7 +21,7 @@ public class WeatherApp {
 
         String urlString = "https://api.open-meteo.com/v1/forecast?" +
                 "latitude=" + latitude + "&longitude=" + longitude +
-                "&hourly=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&timezone=Europe%2FMoscow";
+                "&hourly=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m&timezone=Europe%2FMoscow";
 
         try {
             HttpURLConnection connection = fetchApiResponse(urlString);
